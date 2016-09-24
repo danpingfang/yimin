386e6047426fa78b9be8437df3c91517fbc53b3e
wushaojie <wushaojie@live.com>
2016-09-24 19:49:28 +0800

    样式修改。

--- s.sh
+++ s.sh
@@ -1,5 +1,5 @@
 if [ $1 = "publish" ]; then
-      scp -r static/* root@115.159.33.205:/var/www/app/static/item/
+      scp -r static/* root@115.159.33.205:/var/www/oversealive/s/static/item/
     else
       scp -r static/* root@115.159.33.205:/var/www/app/test/static/item/
 fi
